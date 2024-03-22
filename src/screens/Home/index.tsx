adebd39351e4c5ab89e6c76121bcf4470f2bc71d
CaioMiranda12 <caioclm04@gmail.com>
2024-03-22 11:54:27 -0300

    15 update

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,9 @@
 import { InputMask } from '@react-input/mask';
 
-import { Button } from '../../components/button';
 import { ButtonIcon } from '../../components/button-icon';
 import { Card } from '../../components/card';
 import { CreateCategoryDialog } from '../../components/create-category-dialog';
+import { CreateTransactionDialog } from '../../components/create-transaction-dialog copy';
 import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
 import { Title } from '../../components/title';
@@ -30,7 +30,7 @@ export function Home() {
         <Logo />
 
         <div>
-          <Button>Nova transação</Button>
+          <CreateTransactionDialog />
           <CreateCategoryDialog />
         </div>
       </Header>
